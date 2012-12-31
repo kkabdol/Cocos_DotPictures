3dacf927a62e7a4360be68ffcd0c2014ebae746d
Seonghyeon Choe <kkabdol@gmail.com>
2012-12-31 15:33:48 +0900

    Initial Commit

---
+++ DotPictures/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  DotPicturesAppController.h
+//  DotPictures
+//
+//  Created by Seonghyeon Choe on 12/31/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
